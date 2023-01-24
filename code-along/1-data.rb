f72da68e4f23f9c865ecce0037d072fcb19f589a
connorsteiner <connor.steiner@kellogg.northwestern.edu>
2023-01-24 20:11:33 +0000

    Solutions for part 1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -4,16 +4,30 @@
 
 # In Ruby, there are different *types* of data:
 
-# Numbers
+# # Numbers
+# puts 5
+# puts 2
 
-# Perform simple math with numbers
+# # Perform simple math with numbers
+# puts 5 + 2
+# puts 5 - 2.0
+# puts 5.00 / 2.00
+# puts 5 * 2 + 1
 
-# Strings
+# # Strings
+# puts "connor"
 
-# Combine strings together
+# # Combine strings together
+# puts "connor" + + " " + "steiner"
 
 # Variables
+first_name = "connor"
+last_name = "steiner"
 
 # Combine strings and variables
+puts first_name + last_name
+puts "#{first_name}.#{last_name}@kellogg.northwestern.edu"
 
 # String manipulation
+first_name = first_name + ".i"
+puts "#{first_name}.#{last_name}@kellogg.northwestern.edu"
